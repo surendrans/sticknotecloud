46662661f4ca5fdd3eb82ee4c3e034d619447781
Surendrans <surendrans86@gmail.com>
2012-01-05 10:09:34 +0530

    removed prototype scripts

--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -1,11 +1,30 @@
 var AppRouter = Backbone.Router.extend({
 	
 		routes :{
-				"" : "notes_index"
+				"" : "notes_index",
+				"/" : "notes_index",
+				"search_by_desc": "searchbyTitle"
 		},
-		
 		notes_index : function(){
 		  $.getJSON('/notes', function(data) {
+			  	if (data) {
+			  			var notes = _(data).map(function(v) { return new Note(v.note) });
+			  			new NoteIndex(notes);
+			  	}
+		  });
+		},
+		
+		searchbyTitle : function() {
+			$.getJSON('/notes/search_by_desc?q=' + $('#desc_search').val(), function(data) {
+			  	if (data) {
+			  			var notes = _(data).map(function(v) { return new Note(v.note) });
+			  			new NoteIndex(notes);
+			  	}
+		  });
+		},
+		
+		searchbyTag : function() {
+			$.getJSON('/notes/search_by_tag?q=' + $('#tag_search').val(), function(data) {
 			  	if (data) {
 			  			var notes = _(data).map(function(v) { return new Note(v.note) });
 			  			new NoteIndex(notes);
@@ -14,5 +33,5 @@ var AppRouter = Backbone.Router.extend({
 		}
 		
 });
- var app_router = new AppRouter;
-    Backbone.history.start({pushState: true});
+ var app_router = new AppRouter();
+  Backbone.history.start({pushState: true});
